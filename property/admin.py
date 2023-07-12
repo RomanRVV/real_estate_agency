545602b6e0dbedb6fc3448f7a627435287cdd83f
RomanRVV <rvachyov.roma@yandex.ru>
2023-07-12 19:14:23 +0300

    setup filtering in admin UI

--- property/admin.py
+++ property/admin.py
@@ -12,6 +12,7 @@ class AuthorAdmin(admin.ModelAdmin):
                     'construction_year',
                     'town')
     list_editable = ['new_building']
+    list_filter = ('new_building', 'rooms_number', 'has_balcony', 'active')
 
 
 admin.site.register(Flat, AuthorAdmin)
