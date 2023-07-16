7928bbf3da127200937c62288a807e3c8a2f95c2
RomanRVV <rvachyov.roma@yandex.ru>
2023-07-16 13:36:33 +0300

    Use @register

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class ConnectionInline(admin.TabularInline):
     raw_id_fields = ['owner']
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address')
     readonly_fields = ['created_at']
@@ -24,17 +25,14 @@ class FlatAdmin(admin.ModelAdmin):
     ]
 
 
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     list_display = ('flat', 'author')
     raw_id_fields = ('author', 'flat')
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     search_fields = ('name', 'phonenumber', 'pure_phone', 'flats')
     list_display = ('name', 'phonenumber', 'pure_phone')
     raw_id_fields = ['flats']
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
